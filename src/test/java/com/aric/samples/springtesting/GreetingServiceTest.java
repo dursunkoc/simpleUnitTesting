8a4092dc9bfaab0499e2c7de53ee4b1e4ba5791a
dursunkoc <dursunKoc@gmail.com>
2016-10-16 14:57:10 +0300

    Spring Database Test Completed

--- src/test/java/com/aric/samples/springtesting/GreetingServiceTest.java
+++ src/test/java/com/aric/samples/springtesting/GreetingServiceTest.java
@@ -1,20 +1,41 @@
 package com.aric.samples.springtesting;
 
+import java.util.Optional;
+
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GreetingServiceTest {
+    private static Person CEMIL;
     private static final String FIRST_NAME = "Cemil";
     private static final String LAST_NAME = "Tekin";
 
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        CEMIL = new Person();
+        CEMIL.setFirstName(FIRST_NAME);
+        CEMIL.setLastName(LAST_NAME);
+    }
+
     @Autowired
     private GreetingService greetingService;
+
+    @Before
+    public void setUp() {
+        final PersonRepository mockPersonRepository = Mockito.mock(PersonRepository.class);
+        ReflectionTestUtils.setField(greetingService, "personRepository", mockPersonRepository);
+        Mockito.when(mockPersonRepository.findById(1l)).thenReturn(Optional.of(CEMIL));
+    }
 
     @Test
     public void testGreetShouldReturnAValidGreetingObjectWhenANameIsProvided() throws Exception {
